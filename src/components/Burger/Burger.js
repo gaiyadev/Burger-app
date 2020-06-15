37a76e1121d571a7e563a48388c444de1d3b54b3
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-06-15 18:23:05 +0100

    Bug: fixed bug in BurgerBuilder.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import styles from '../../components/Burger/BurgerIngredient/Burger.module.css';
 import BurgerIngredient from '../../components/Burger/BurgerIngredient/Burgeringredient';
 
 const burger = (props) => {
+    const BIngredient = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        });
+    });
+
     return (
         <div className={styles.Burger} >
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {BIngredient}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,23 @@ import Burger from '../../components/Burger/Burger';
 
 // Burger builder component
 class BurgerBuilder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ingredients: {
+                salad: 1,
+                bacon: 1,
+                cheese: 1,
+                meat: 2,
+            }
+        }
+    }
+
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build controls</div>
             </Aux>
         );
     }
