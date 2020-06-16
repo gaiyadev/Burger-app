4df5728c43191c38bd702231bce5cda8fa95cc68
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-06-16 20:08:43 +0100

    Add: added sidedrawer to the app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxillary';
 import styles from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toobar';
 import SideDrawer from '../Navigation/SideDrawer/Sidedrawer';
 
-const layout = (props) => (
-    <Aux>
-        <div>
-            <Toolbar></Toolbar>
-            <SideDrawer></SideDrawer>
-        </div>
-        <main className={styles.Content}>
-            {props.children}
-        </main>
-    </Aux>
+class Layout extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            showDrawer: false
+        }
+    }
+    SideDrawerHandler = () => {
+        this.setState({ showDrawer: true });
+    }
+    render() {
+        return (
+            <Aux>
+                <div>
+                    <Toolbar></Toolbar>
+                    <SideDrawer open={this.state.showDrawer}
+                        closed={this.SideDrawerHandler} >
+                    </SideDrawer>
+                </div>
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
 
-);
-export default layout;
+        );
+    }
+}
+export default Layout;
 
 //Application layout file
--- src/components/Navigation/SideDrawer/Sidedrawer.js
+++ src/components/Navigation/SideDrawer/Sidedrawer.js
@@ -2,16 +2,20 @@ import React from 'react'
 import NavbarItems from '../Navbar/Navbar';
 import Logo from '../../Logo/Logo';
 import styles from '../SideDrawer/Sidedrawer.module.css';
-
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxillary';
 
 const sideDrawer = (props) => {
     return (
-        <div className={styles.SideDrawer} >
-            <Logo height="11%" />
-            <nav>
-                <NavbarItems />
-            </nav>
-        </div>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={styles.SideDrawer} >
+                <Logo height="11%" />
+                <nav>
+                    <NavbarItems />
+                </nav>
+            </div>
+        </Aux>
     );
 }
 export default sideDrawer;
